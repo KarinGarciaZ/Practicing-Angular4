9691badc714508a98c768cac086cb42658948f9e
KarinGarciaZ <oscarin962011@hotmail.com>
2017-10-19 20:45:26 -0500

    Added a component and a service of Post.

--- src/app/post-component/post-component.component.ts
+++ src/app/post-component/post-component.component.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,14 +6,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './post-component.component.html',
   styleUrls: ['./post-component.component.css']
 })
-export class PostComponentComponent{
+export class PostComponentComponent implements OnInit{
   posts: any[];
-  private url = 'http://jsonplaceholder.typicode.com/posts';
   
-  constructor(private http: Http) {
-    http.get(this.url)
+  constructor(private service: PostService) {
+    
+  }
+
+  ngOnInit(){
+    this.service.getPosts()
     .subscribe(response =>{
       this.posts = response.json();
+    }, error =>{
+      alert('Something happened');
+      console.log(error);
     });
   }
 
@@ -21,10 +27,40 @@ export class PostComponentComponent{
     let post = {title: input.value};
     input.value = '';
 
-    this.http.post(this.url, JSON.stringify(post))
+    this.service.createPost(post)
     .subscribe(response => {
       post['id'] = response.json().id;
       this.posts.splice(0,0,post);
+    }, (error: Response) =>{
+      if (error.status === 400){}
+        //this.form.setErrors(error.json());
+      else{        
+      alert('Something happened');
+      console.log(error);
+      }
     });
+  }
+
+  updatePost(post){
+    this.service.updatePost(post)
+      .subscribe(response =>{
+        console.log(response.json());
+      }, error =>{
+        alert('Something happened');
+        console.log(error);
+      });
+  }
+
+  deletePost(post){
+    this.service.deletePost(post.id)
+      .subscribe(
+        response =>{
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index,1);
+        }, 
+        (error: Response) =>{
+          (error.status === 404)? alert('Error 404'): alert('Something happened');
+          console.log(error);
+      });
   }
 }
